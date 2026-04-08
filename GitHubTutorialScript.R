25e62c34494fa186e8e5a72a387a880b8fb3c572
kmblincow <kmblincow@gmail.com>
2026-04-08 10:09:52 -0400

    Create the demo script file

---
+++ GitHubTutorialScript.R
@@ -0,0 +1,21 @@
+#INSERT YOUR NAME HERE
+#INSERT DATE HERE
+
+#clear my workspace
+rm(list = ls())
+
+#load packages
+install.packages("learnr")
+library(learnr)
+
+#Generate random praise
+random_praise()
+#ADD COMMENT WITH YOUR RANDOM PRAISE PHRASE BELOW:
+
+#Generate random encouragement
+random_encouragement()
+#ADD COMMENT WITH YOUR RANDOM ENCOURAGEMENT PHRASE BELOW:
+
+# Save this R script file as "GitHubTutorialScript_YOURNAME.R"
+# commit and push it to the GitHub repository
+
